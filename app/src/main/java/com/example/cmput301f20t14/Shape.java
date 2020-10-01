3639a90fb261bcefcaf900acdd9269c05f06fb40
Olivier Vadiavaloo <vadiaval@ualberta.ca>
2020-09-30 23:15:38 -0600

    Style(Shape): remove unnecessary symbols

--- app/src/main/java/com/example/cmput301f20t14/Shape.java
+++ app/src/main/java/com/example/cmput301f20t14/Shape.java
@@ -3,11 +3,8 @@ package com.example.cmput301f20t14;
 public abstract class Shape {
     int x;
     int y;
-<<<<<<< HEAD
     String color1 = "blue"
-=======
     String color2 = "red";
->>>>>>> d05807bfcadfb2e9d5074f5ddf4917c5d357190e
 
     public Shape() {
 
