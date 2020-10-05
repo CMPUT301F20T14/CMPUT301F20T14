432c87da2ca3fa1a1fca739181e7e461111d69b3
Faiyaz Ahmed <faiyaz1@ualberta.ca>
2020-10-04 20:04:41 -0600

    Resolved conflict

--- app/src/main/java/com/example/cmput301f20t14/Shape.java
+++ app/src/main/java/com/example/cmput301f20t14/Shape.java
@@ -3,10 +3,8 @@ package com.example.cmput301f20t14;
 public abstract class Shape {
     int x;
     int y;
-<<<<<<< HEAD
-=======
     String color = "Blue";
->>>>>>> b462c09beaea04b473ab6deb055158c70688a07f
+
 
     public Shape() {
 
